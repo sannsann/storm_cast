4516ccebeb9455834e28a0037fb57b72e1aa3920
Sann Chhan <sanncchhan@gmail.com>
2013-07-10 20:08:04 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+StormCast::Application.config.secret_token = 'b8f2127c232c3b854a8441b71b22a008e105080e109fff6b82a4a7fcf46df97d87e68c974f3bd76411846b3a77a7d491aa5b4dcf855af4e656557cab2fa1d36e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+StormCast::Application.config.session_store :cookie_store, key: '_storm_cast_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# StormCast::Application.config.session_store :active_record_store
